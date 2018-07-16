d68168a4c471953048f4a9ec094cb6633d82a574
Junwoo Hwang <capollux10@gmail.com>
2018-07-16 22:48:02 +0900

    Add copy-to-clipboard using clipboardjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require clipboard.min
 //= require_tree .
--- app/assets/javascripts/urls.js
+++ app/assets/javascripts/urls.js
@@ -1,18 +1,16 @@
 document.addEventListener('turbolinks:load', function(e) {
   var originInput = document.getElementById('url_origin');
-  originInput.addEventListener('focus', function(e){
+  originInput && originInput.addEventListener('focus', function(e){
     this.parentElement.parentElement.classList.remove('active');
   });
-
-  var copyToClipboard = document.getElementById('copy');
-  copyToClipboard.addEventListener('click', function(e) {
-    var aux = document.createElement("input");
-    aux.setAttribute("value", `https://zzu.li/${document.getElementById('created-shortened').innerHTML}`);
-    document.body.appendChild(aux);
-    aux.select();
-    document.execCommand("copy");
-    document.body.removeChild(aux);
-    alert('Copied!')
+  
+  new ClipboardJS('#copy', {
+    text: function(trigger) {
+        var text = [];
+        while (trigger = trigger.previousElementSibling)
+          text.push(trigger.innerText);
+        return text.reverse().join('');
+    }
   });
 });
 
