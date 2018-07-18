955649d2f623e1285b9812188e74d7362adad92c
Junwoo Hwang <capollux10@gmail.com>
2018-07-18 15:38:06 +0900

    Reject when the destination URL is invalid URL

--- app/models/url.rb
+++ app/models/url.rb
@@ -26,14 +26,28 @@ class Url < ApplicationRecord
     end
 
     def format_of_origin
-      match_data = origin.match(URI::regexp(%w(http https))) || []
-      # 1: scheme, 2: opaque, 3: userinfo, 4: host, 5: port, 6: registry,  7: path, 8: query, 9: fragment
-      if match_data[1].nil? 
-        errors.add :origin, 'Must start with \'http://\' or \'https://\'.'
-      elsif match_data[4].nil?
-        errors.add :origin, 'Host can\'t be blank.'
-      elsif match_data[7] == "/#{shortened}" && match_data[4] == 'zzu.li'
-        errors.add :origin, 'Can\'t be the same as the shortened URL.'
-      end
+      begin  
+        uri = URI.parse(origin)
+      rescue Exception => e
+        errors.add :origin, e.message
+      else
+        if %w(http https).exclude?(uri.scheme)
+          errors.add :origin, 'Must start with \'http://\' or \'https://\'.'
+        elsif uri.host.nil?
+          errors.add :origin, 'Host can\'t be blank.'
+        elsif uri.path == "/#{shortened}" && uri.host == 'zzu.li'
+          errors.add :origin, 'Can\'t be the same as the shortened URL.'
+        end
+      end  
+
+      # match_data = origin.match(URI::regexp(%w(http https))) || []
+      # if match_data[1].nil? 
+      #   errors.add :origin, 'Must start with \'http://\' or \'https://\'.'
+      # elsif match_data[4].nil?
+      #   errors.add :origin, 'Host can\'t be blank.'
+      # elsif match_data[7] == "/#{shortened}" && match_data[4] == 'zzu.li'
+      #   errors.add :origin, 'Can\'t be the same as the shortened URL.'
+      # end
+      # 1: scheme, 2: opaque, 3: userinfo, 4: host, 5: port, 6: registry, 7: path, 8: query, 9: fragment
     end
 end
